b93d43dce6932b440e01ef3d95974bde5e3d6546
Douglas Batista <dobatista7@gmail.com>
2024-07-01 21:12:09 -0300

    criacao da classe abstrata Lesson com o método abstrato duration

---
+++ entities/Lesson.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace desafioPlataformaEnsinoCsharp.entities
+{
+    abstract class Lesson
+    {
+        public string Title { get; set; }
+        protected Lesson(string title)
+        {
+            Title = title;
+        
+        }
+        public abstract int Duration();
+    }
+}
