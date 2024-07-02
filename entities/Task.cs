e2b8acb83ab983d77ec2c5051dbe2e22b0365fe3
Douglas Batista <dobatista7@gmail.com>
2024-07-01 21:20:30 -0300

    criacao da classe Task que estende da Classe Lesson com o método duration

---
+++ entities/Task.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace desafioPlataformaEnsinoCsharp.entities
+{
+    class Task : Lesson
+    {
+        public string Description { get; set; }
+        public int QuestionCount { get; set; }
+
+        public Task(string title,string description, int questionCount)
+            :base(title)
+        {
+            Description = description;
+            QuestionCount = questionCount;
+        }
+
+        public override int Duration()
+        {
+            return QuestionCount * 5 * 60;
+        }
+    }
+}
