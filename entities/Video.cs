6e13077becba43b39285f861102d8ef953dc4fdc
Douglas Batista <dobatista7@gmail.com>
2024-07-01 21:28:26 -0300

    criacao da classe Video que estende da Classe lesson com o método sobrescrito na subclasse

---
+++ entities/Video.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace desafioPlataformaEnsinoCsharp.entities
+{
+    class Video : Lesson
+    {
+        public string URL {  get; set; }
+        public int DurationInSeconds { get; set; }
+
+        public Video(string title, string url,int durationInSeconds)
+            : base(title)
+        {
+            URL = url;
+            DurationInSeconds = durationInSeconds;
+        }
+
+        public override int Duration()
+        {
+            return DurationInSeconds;
+
+        }
+    }
+}
